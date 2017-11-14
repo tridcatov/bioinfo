d79da4e893aaa22c55ece4d0e54ec277504a55a4
Anastasia Zotova <ashunaeva@mail.ru>
2017-11-14 20:34:38 +0300

    Transformed geneadder into console utility

--- geneadder.py
+++ geneadder.py
@@ -1,20 +1,18 @@
-#!/bin/python
+#!/usr/bin/python
 
 import sys
 import string
 
-input = sys.argv[1]
 dict = {}
 totalCount = 0
 
-for line in open(input, 'r'):
+for line in sys.stdin:
 	[_, gene, count] = line.split("\t")
 
 	if ( not count.isdigit()):
 		try:
 			count = float(count)
 		except:
-			print("Not a float, possible header")
 			continue
 	else:
 		count = int(count)
@@ -27,12 +25,10 @@ for line in open(input, 'r'):
 
 genes = sorted(dict, key=dict.__getitem__, reverse=True)
 
-output = input + ".summed.txt"
-file = open(output, 'w')
 
 for gene in genes:
 	count = dict[gene]
-	line = gene + '\t' + str(count) + '\t' + str(totalCount) + '\n'
+	line = gene + '\t' + str(int(count)) + '\t' + str(int(totalCount))
 	totalCount -= count
-	file.write(line)
+	print(line)
 	
