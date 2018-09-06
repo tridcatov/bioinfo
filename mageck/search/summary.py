0845c6688c3140f9d283aba365db9a90b42827fd
Ivan Zotov <tridcatov@gmail.com>
2018-09-06 21:13:21 +0300

    Added gene search in summary tables

---
+++ mageck/search/summary.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Analyze input ranked genes tables')
+parser.add_argument('files', metavar='file', type=str, nargs='+', help='Input files')
+
+args = parser.parse_args()
+
+files = args.files
+
+cache = {}
+fc = {}
+
+import csv
+
+for f in files:
+    with open(f, 'r') as content:
+        reader = csv.reader(content, delimiter= '\t')
+        print('Analyzing ' + f)
+        file = len(fc) + 1
+        fc[file] = f
+        count = 0
+        for row in reader:
+            if count == 0:
+                count += 1
+                continue
+
+            gene = row[0]
+            if ( gene.startswith('hsa-mir') or gene.startswith('Non')):
+                continue
+
+            if not gene in cache:
+                cache[gene] = []
+
+            pvalue = float(row[9])
+            rank = float(row[11])
+            cache[gene].append((file, rank, pvalue))
+            count += 1
+			
+        print('Loaded ' + str(count) + ' genes')
+		
+for gene in sys.stdin:
+    gene = gene.strip()
+    if not gene in cache:
+        print("No gene {} in cache".format(gene))
+        continue
+
+    for info in cache[gene]:
+        file, rank, pvalue = info
+        print("Rank {} with pvalue {} in sample {}".format(rank, pvalue, fc[file]))
